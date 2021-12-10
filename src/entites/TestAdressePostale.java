16444d9b689859b018aa2ceb201616aadf5d4406
Nicolas Moreau <nicolas.moreau93@orange.fr>
2021-12-10 14:36:07 +0100

    correction de TestAdressePostale

--- src/entites/TestAdressePostale.java
+++ src/entites/TestAdressePostale.java
@@ -6,16 +6,6 @@ public class TestAdressePostale {
 		
 		AdressePostale aP1 = new AdressePostale();
 		AdressePostale aP2 = new AdressePostale();
-		
-		//aP1.numRue = 22;
-		//aP1.libRue = "rue du Château";
-		//aP1.codePostal = 44300;
-		//aP1.ville = "Nantes";
-		
-		//aP2.numRue = 78;
-		//aP2.libRue = "rue du Guillet";
-		//aP2.codePostal = 85000;
-		//aP2.ville = "les Sables d'Olonne";
 	}
 
 }
