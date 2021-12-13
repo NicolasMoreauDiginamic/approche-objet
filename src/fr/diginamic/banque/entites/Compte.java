fb2a1024512b1dfeaaaee3b4557adc1100fb1b1c
Nicolas Moreau <nicolas.moreau93@orange.fr>
2021-12-13 14:04:48 +0100

    Commit TP5

---
+++ src/fr/diginamic/banque/entites/Compte.java
@@ -0,0 +1,29 @@
+package fr.diginamic.banque.entites;
+
+public class Compte {
+	
+	String numCompte;
+	double soldeCompte;
+	
+	
+	
+	public Compte(String numCompte, double soldeCompte) {
+		this.numCompte = numCompte;
+		this.soldeCompte = soldeCompte;
+	}
+	
+	public String getNumCompte() {
+		return numCompte;
+	}
+	public void setNumCompte(String numCompte) {
+		this.numCompte = numCompte;
+	}
+	public double getSoldeCompte() {
+		return soldeCompte;
+	}
+	public void setSoldeCompte(float soldeCompte) {
+		this.soldeCompte = soldeCompte;
+	}
+	
+	
+}
---
+++ src/fr/diginamic/banque/entites/TestBanque.java
@@ -0,0 +1,13 @@
+package fr.diginamic.banque.entites;
+
+public class TestBanque {
+
+	public static void main(String[] args) {
+		
+		Compte compte1 = new Compte("001B78",14.865);
+		
+		System.out.println(compte1);
+
+	}
+
+}
